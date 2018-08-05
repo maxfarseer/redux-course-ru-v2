6acba912e2da14cebd10889d5098dd64594cf35d
Max P <maxfarseer@gmail.com>
2018-08-05 14:19:16 +0300

    add (store) logger middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { rootReducer } from '../reducers'
+import logger from 'redux-logger'
 
-// удалили "начальное состояние = initial state"
-// так как теперь наш редьюсер составной,
-// и нам нужны initialState каждого редьюсера.
-// Это будет сделано автоматически.
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(logger))
