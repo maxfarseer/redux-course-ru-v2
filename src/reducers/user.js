e113b7a49c6629475e4ed4501afa031026e068a6
Artyom Bochkov <artem.bochkov@valant.com>
2018-08-09 22:58:52 +0300

    united the same switch-cases

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,31 +9,23 @@ const initialState = {
 export function userReducer(state = initialState, action) {
   switch (action.type) {
     case t.LOGIN_REQUEST:
+    case t.LOGIN_STATUS_REQUEST:
+    case t.LOGOUT_REQUEST:
       return { ...state, isFetching: true, error: '' }
 
-    case t.LOGIN_SUCCES:
+    case t.LOGIN_SUCCESS:
+    case t.LOGIN_STATUS_SUCCESS:
       return { ...state, isFetching: false, name: action.payload }
 
     case t.LOGIN_FAIL:
+    case t.LOGOUT_FAIL:
       return { ...state, isFetching: false, error: action.payload.message }
-
-    case t.LOGIN_STATUS_REQUEST:
-      return { ...state, isFetching: true, error: '' }
-
-    case t.LOGIN_STATUS_SUCCESS:
-      return { ...state, isFetching: false, error: '', name: action.payload }
 
     case t.LOGIN_STATUS_FAIL:
       return { ...state, isFetching: false }
 
-    case t.LOGOUT_REQUEST:
-      return { ...state, isFetching: true, error: '' }
-
     case t.LOGOUT_SUCCESS:
       return initialState
-
-    case t.LOGOUT_FAIL:
-      return { ...state, isFetching: false, error: action.payload.message }
 
     default:
       return state
