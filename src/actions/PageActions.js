f36b26a92df53a147709172012444d95db7aabd8
Max P <maxfarseer@gmail.com>
2018-08-18 13:19:25 +0300

    fix (Page) incorrect condition for load all photos recursively

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -29,7 +29,7 @@ function getMorePhotos(offset, count, year, dispatch) {
     r => {
       try {
         photosArr = photosArr.concat(r.response.items)
-        if (offset <= r.response.items.length - count) {
+        if (offset <= r.response.count) {
           offset += 200 // максимальное количество фото которое можно получить за 1 запрос
           getMorePhotos(offset, count, year, dispatch)
         } else {
